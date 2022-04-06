6e38acce54f99a25c4ffbba4d7ec75778c83c56b
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-04-06 18:59:01 +0200

    Added more JavaScript li's

--- js/script.js
+++ js/script.js
@@ -1,49 +1,59 @@
-// Header date:
+/*----------Header date----------*/
 n = new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
 document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
 
-// Get the modal
+
+/*----------Modal----------*/
+//variables
 let modal = document.getElementById("myModal");
-// Get the button that opens the modal
 let btn = document.querySelector(".myBtn");
-// Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal
+
+//functions
+//display modal function
 btn.onclick = function () {
   modal.style.display = "block";
 };
-// When the user clicks on <span> (x), close the modal
+
+//close modal function
 span.onclick = function () {
   modal.style.display = "none";
 };
-// When the user clicks anywhere outside of the modal, close it
+
+//window close modal function
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
 
-//selectors
+
+/*----------Form----------*/
+//variables
 const todoInput = document.querySelector(".todo_input");
+const dateInput = document.querySelector(".date_input");
+const timeInput = document.querySelector(".time_input");
 const todoButton = document.querySelector(".todo_button");
 const todoList = document.querySelector(".todo_list");
 const filterOption = document.querySelector(".filter_todo");
+
 //event listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("click", filterTodo);
-//functions
 
+//functions
+//todo container function
 function addTodo(event) {
   event.preventDefault();
-  //todo DIV
+  //todo div
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
 
-  //todo LI
+  //task name li
   const newTodo = document.createElement("li");
   newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo_item");
@@ -52,59 +62,78 @@ function addTodo(event) {
     return null;
   }
 
-  //complete BUTTON
+  //due date li
+  const dateTodo = document.createElement("li");
+  dateTodo.innerText = dateInput.value;
+  dateTodo.classList.add("todo_item");
+  todoDiv.appendChild(dateTodo);
+  if (dateInput.value === "") {
+    return null;
+  }
+
+  //due time li
+  const timeTodo = document.createElement("li");
+  timeTodo.innerText = timeInput.value;
+  timeTodo.classList.add("todo_item");
+  todoDiv.appendChild(timeTodo);
+  if (timeInput.value === "") {
+    return null;
+  }
+
+  //complete button
   const completedButton = document.createElement("button");
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
   completedButton.classList.add("complete_btn");
   todoDiv.appendChild(completedButton);
 
-  //edit BUTTON
+  //edit button
   const editButton = document.createElement("button");
   editButton.innerHTML = '<i class="fas fa-pen"></i>';
   editButton.classList.add("edit_btn");
   todoDiv.appendChild(editButton);
 
-  //delete BUTTON
+  //delete button
   const deleteButton = document.createElement("button");
   deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
   deleteButton.classList.add("delete_btn");
   todoDiv.appendChild(deleteButton);
 
-  //Append to Actual LIST
+  //append to list
   todoList.appendChild(todoDiv);
-  //Clear todo input VALUE
+
+  //Clear input values
   todoInput.value = "";
+  dateInput.value = "";
+  timeInput.value = "";
 }
 
-
-//DELETE & CHECK
+//check, edit and delete function
 function deleteCheck(e) {
   const item = e.target;
-  //DELETE ITEM
+  //delete item
   if (item.classList[0] === "delete_btn") {
     const todo = item.parentElement;
-    //ANIMATION TRANSITION
+    //delete animation
     todo.classList.add("fall");
     todo.addEventListener("transitionend", function () {
       todo.remove();
     });
   }
 
-  //COMPLETE ITEM
+  //check item off
   if (item.classList[0] === "complete_btn") {
     const todo = item.parentElement;
     todo.classList.toggle("completedItem");
   }
 
-  //EDIT ITEM
+  //edit existing item
   if (item.classList[0] === "edit_btn") {
     const todo = item.parentElement;
     todo.classList.toggle("completedItem");
   }
 }
 
-
-//FILTERING THE TASKS ACCORDING THE OPTION
+//task filtering function
 function filterTodo(e) {
   const todos = todoList.childNodes;
   for (let i = 1; i < todos.length; i++) {
