ec38cc627fac093916b4af76063530debec4308e
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-04-06 19:11:21 +0200

    push

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
 //variables
 let modal = document.getElementById("myModal");
 let btn = document.querySelector(".myBtn");
-let span = document.getElementsByClassName("close")[0];
+let span = document.querySelector(".close")[0];
 
 //functions
 //display modal function
